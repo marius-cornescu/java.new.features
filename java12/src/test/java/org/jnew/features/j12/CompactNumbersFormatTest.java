0ae28ef606f7380a45d065a425250920e2a065a0
CornescuMA <marius.cornescu@rabobank.com>
2022-01-27 17:35:27 +0100

    java 12

---
+++ java12/src/test/java/org/jnew/features/j12/CompactNumbersFormatTest.java
@@ -0,0 +1,52 @@
+package org.jnew.features.j12;
+
+import org.junit.jupiter.api.Test;
+
+import java.text.NumberFormat;
+import java.util.Locale;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class CompactNumbersFormatTest {
+
+    @Test
+    void compactNumberFormat() {
+        // given
+        NumberFormat shortNF = NumberFormat.getCompactNumberInstance();
+        NumberFormat shortNF2 = NumberFormat.getCompactNumberInstance();
+        shortNF2.setMaximumFractionDigits(2);
+
+        // when
+        String actual1000 = shortNF.format(1_000);
+        String actual1Mil = shortNF.format(1_000_000);
+        String actual2000 = shortNF.format(1_500);
+        String actual1k5 = shortNF2.format(1_500);
+
+        //then
+        assertEquals("1K", actual1000);
+        assertEquals("1M", actual1Mil);
+        assertEquals("2K", actual2000);
+        assertEquals("1.5K", actual1k5);
+    }
+
+    @Test
+    void compactNumberFormatNlLocale() {
+        // given
+        NumberFormat shortNF = NumberFormat.getCompactNumberInstance(Locale.GERMAN, NumberFormat.Style.SHORT);
+        NumberFormat shortNF2 = NumberFormat.getCompactNumberInstance();
+        shortNF2.setMaximumFractionDigits(2);
+
+        // when
+        String actual1000 = shortNF.format(1_000);
+        String actual1Mil = shortNF.format(1_000_000);
+        String actual2000 = shortNF.format(1_500);
+        String actual1k5 = shortNF2.format(1_500);
+
+        //then
+        assertEquals("1.000", actual1000);
+        assertEquals("1 Mio.", actual1Mil);
+        assertEquals("2K", actual2000);
+        assertEquals("1.5K", actual1k5);
+    }
+
+}
--- java12/src/test/java/org/jnew/features/j12/StringMethodsTest.java
+++ java12/src/test/java/org/jnew/features/j12/StringMethodsTest.java
@@ -34,4 +34,20 @@ class StringMethodsTest {
                 "     Text\n", actual);
     }
 
+    @Test
+    void transformMethod() {
+        // given
+        var singleText = "This \nIs\r\nA\nMixed\r\nText\n";
+
+        // when
+        long actual = singleText
+                .transform(s -> s.replace('s', '$'))
+                .transform(String::toUpperCase)
+                .transform(String::lines)
+                .count();
+
+        //then
+        assertEquals(5, actual);
+    }
+
 }
