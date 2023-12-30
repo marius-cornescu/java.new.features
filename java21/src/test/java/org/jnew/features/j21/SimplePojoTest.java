99b66d72b74a847895140b4a35fa41feadb55b01
Marius Cornescu <marius_cornescu@yahoo.com>
2023-12-30 08:38:18 +0100

    Java 21 play

---
+++ java21/src/test/java/org/jnew/features/j21/SimplePojoTest.java
@@ -0,0 +1,17 @@
+package org.jnew.features.j21;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SimplePojoTest {
+
+    @Test void testConstructor() {
+        // given
+
+        // when
+
+        // then
+    }
+
+}
