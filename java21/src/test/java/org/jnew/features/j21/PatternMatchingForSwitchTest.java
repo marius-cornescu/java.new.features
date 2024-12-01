5ecb420ebde039040952cb41e283586f9920958a
Marius Cornescu <marius_cornescu@yahoo.com>
2024-12-01 20:39:26 +0100

    Java 21 play

---
+++ java21/src/test/java/org/jnew/features/j21/PatternMatchingForSwitchTest.java
@@ -0,0 +1,30 @@
+package org.jnew.features.j21;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.SequencedCollection;
+import java.util.TreeSet;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.params.provider.Arguments.of;
+
+@Slf4j
+class PatternMatchingForSwitchTest {
+
+    @Test
+    void test() {
+        // given
+
+        // when
+
+        // then
+    }
+
+}
--- java21/src/test/java/org/jnew/features/j21/SequencedCollectionsTest.java
+++ java21/src/test/java/org/jnew/features/j21/SequencedCollectionsTest.java
@@ -1,26 +1,53 @@
 package org.jnew.features.j21;
 
 import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.*;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.params.provider.Arguments.of;
 
 @Slf4j
 class SequencedCollectionsTest {
 
+    static Stream<Arguments> variousCollectionValues() {
+        var list = new ArrayList<>();
+        list.add("FIRST");
+        list.add("SECOND");
+        list.add("LAST");
+
+        var dequeue = new LinkedList<>();
+        dequeue.add("FIRST");
+        dequeue.add("SECOND");
+        dequeue.add("LAST");
+
+        var sortedSet = new TreeSet<>();
+        sortedSet.add("FIRST");
+        sortedSet.add("SECOND");
+        sortedSet.add("LAST");
+
+        return Stream.of(
+                of(list, "LAST"),
+                of(dequeue, "LAST"),
+                of(dequeue.reversed(), "FIRST"),
+                of(sortedSet, "SECOND")
+        );
+    }
+
     @ParameterizedTest
-    @CsvSource({
-            "A, B",
-            "C, D",
-    })
-    void test(String value, String expectedResult) {
+    @MethodSource("variousCollectionValues")
+    void lastElementOfCollectionsIs(SequencedCollection<String> collection, String expectedResult) {
         // given
 
         // when
+        var lastElement = collection.getLast();
 
         // then
+        assertEquals(expectedResult, lastElement);
     }
 
 }
